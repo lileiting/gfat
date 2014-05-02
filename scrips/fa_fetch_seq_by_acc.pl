5c8cd8fc30bb8a4557b2f95f5be2acf1bc35193c
Lei-Ting Li <lileiting@gmail.com>
2014-05-02 13:46:37 +0800

    Create fa_fetch_seq_by_acc.pl

---
+++ scrips/fa_fetch_seq_by_acc.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use Bio::Perl;
+use File::Basename;
+use Getopt::Long;
+
+sub usage{
+	die "Usage: ",basename($0), 
+" [-h|?] [-d DB] [-f FORMAT] [-a ACC[,ACC ...]] LISTFILE [LISTFILE ...]
+Supported databases (Default: genbank):
+    genbank
+    genpept
+    swiss
+    embl
+    refseq
+
+Default format:fasta
+Other formats please refer to 
+http://www.bioperl.org/wiki/HOWTO:SeqIO#Formats
+	\n";
+}
+&usage unless @ARGV;
+
+my $help = 0;
+my $db = 'genbank';
+my $format = 'fasta';
+my @acc = ();
+my @files;
+
+GetOptions("help|?"     =>  \$help,
+           "db=s"       =>  \$db,
+           "format=s"   =>  \$format,
+           "acc=s"      =>  \@acc);
+
+&usage if($help);
+&usage unless($db =~ /genbank|genpept|swiss|embl|refseq/i);
+@acc = split(/,/,join(",", @acc));
+@files = @ARGV;
+&usage unless(@acc or @files);
+
+my @seqs;
+if(@acc){
+	for(@acc){
+		warn "Fetching $_ ...\n";
+		push @seqs, get_sequence($db, $_);
+	}
+}
+for my $file (@files){
+	open my $fh, $file or die "ERROR in opening file $file ...\n";
+	while(<$fh>){
+		next if /^\s*$/ or /^\s*#/;
+		chomp;
+		warn "Fetching $_ ...\n";
+		push @seqs, get_sequence($db, $_);
+	}
+	close $fh;
+}
+
+write_sequence(">-", $format, @seqs);
