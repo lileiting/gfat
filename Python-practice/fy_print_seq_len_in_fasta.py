1371f1a1b2914a7f2e328f69bdc599c1eada54db
Lei-Ting Li <lileiting@gmail.com>
2014-11-11 14:43:29 +0800

    Add the statistics information of sequence length
    Include number of sequences, total length, maximum length, and minimum
    length

--- Python-practice/fy_print_seq_len_in_fasta.py
+++ Python-practice/fy_print_seq_len_in_fasta.py
@@ -16,5 +16,20 @@ if len(sys.argv) < 2:
     sys.exit()
 
 from Bio import SeqIO
+seqlen     = []
+num_of_seq = 0
+total_len  = 0
 for record in SeqIO.parse(sys.argv[1], 'fasta'):
     print("%s %i" % (record.id, len(record)))
+    num_of_seq += 1
+    total_len += len(record)
+    seqlen.append(len(record))
+
+seqlen.sort()
+min_len = seqlen[0]
+max_len = seqlen[-1]
+
+print("Number of sequences: " + str(num_of_seq))
+print("Total length: " + str(total_len))
+print("Max length: " + str(max_len))
+print("Min length: " + str(min_len))
