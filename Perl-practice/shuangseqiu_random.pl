161da73cfc615ab7b0b6239b5fc4963dd125746b
Lei-Ting Li <lileiting@gmail.com>
2015-02-16 18:29:43 +0800

    Simplify codes

--- Perl-practice/shuangseqiu_random.pl
+++ Perl-practice/shuangseqiu_random.pl
@@ -8,17 +8,10 @@ sub main{
     my $blue_max = 16;
 
     for(1..100){
-        my @n;
         my %hash;
-        for(1..6){
-            my $red = int(rand($red_max) + 1);
-            redo if $hash{$red};
-            push @n, $red;
-            $hash{$red}++;
-        }
-        @n = sort{$a <=> $b}@n;
-        push @n, int(rand($blue_max) + 1);
-        printf "%4d%4d%4d%4d%4d%4d%4d\n",@n;
+        $hash{int(rand($red_max) + 1)}++ until keys %hash == 6;
+        printf "%4d%4d%4d%4d%4d%4d%4d\n",
+            sort{$a <=> $b}(keys %hash), int(rand($blue_max) + 1);
     }
 }
 
