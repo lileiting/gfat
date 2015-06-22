33e73920949ecf3897e13b45b00997cf524b7611
Leiting Li <lileiting@gmail.com>
2015-06-22 09:54:05 +0800

    add the option of create random length of sequences

--- simulation/randseq.pl
+++ simulation/randseq.pl
@@ -10,14 +10,16 @@ use FormatSeqStr;
 sub usage{
     print <<USAGE;
 
-perl randseq.pl LENGTH [OPTIONS]
+perl randseq.pl [OPTIONS]
 
   Default print nuleotide sequences 100 bp
 
-  -n,--num    Number of sequences [default: 1]
-  -a,--aa     Print amino acid sequences [default print nucleotides]
-  -p,--prefix Prefix of sequence names [default: RandSeq]
-  -h,--help   Print help
+  -n,--num     Number of sequences [default: 1]
+  -a,--aa      Print amino acid sequences [default print nucleotides]
+  -p,--prefix  Prefix of sequence names [default: RandSeq]
+  -I,--min NUM Minimum length of sequence [defalt:100] 
+  -X,--max NUM Maximum length of sequence [defalt:100]
+  -h,--help    Print help
 
 USAGE
     exit;
@@ -27,20 +29,27 @@ sub read_commands{
     my $help;
     my $num = 1;
     my $aa;
-    my $seqlen = 100;
+    #my $seqlen = 100;
     my $prefix = q/RandSeq/;
+    my $min = 100;
+    my $max = 100;
     GetOptions(
-        "n|num=i" => \$num,
-        "a|aa" => \$aa,
+        "n|num=i"    => \$num,
+        "a|aa"       => \$aa,
         "p|prefix=s" => \$prefix,
-        "h|help" => \$help);
+        "I|min=i"    => \$min,
+        "X|max=i"    => \$max,
+        "h|help"     => \$help);
     usage if $help;
-    $seqlen = shift @ARGV if @ARGV;
+    die "ERROR: max length ($max), min length($min)\n" if $max < $min;
+    #$seqlen = shift @ARGV if @ARGV;
     return {
-        num => $num,
-        seqlen => $seqlen,
+        num    => $num,
+       # seqlen => $seqlen,
         prefix => $prefix,
-        aa => $aa
+        min    => $min,
+        max    => $max,
+        aa     => $aa
     };
 }
 
@@ -56,6 +65,17 @@ sub amino_acid{
         S T W Y V/;
 }
 
+sub rand_length{
+    my ($min, $max) = @_;
+    my $length;
+    if($min == $max){
+        $length = $min;
+    }else{
+        $length = $min + int(rand($max - $min + 1));
+    }
+    return $length;
+}
+
 sub rand_seq{
     my ($length, @char) = @_;
     return join('', map{$char[int(rand(scalar(@char)))]}(1..$length));
@@ -64,13 +84,16 @@ sub rand_seq{
 sub main{
     my $para = read_commands;
     my $num = $para->{num};
-    my $length = $para->{seqlen};
+    #my $length = $para->{seqlen};
+    my $min = $para->{min};
+    my $max = $para->{max};
     my $aa = $para->{aa};
     my $prefix = $para->{prefix};
     my ($unit, @char) = nucleotide;
     ($unit, @char) = amino_acid if $aa;
 
     for my $n (1..$num){
+        my $length = rand_length($min, $max);
         my $seqheader = qq/$prefix$n $length$unit/;
         my $seqstr    = format_seqstr(rand_seq($length, @char));
         print ">$seqheader\n$seqstr\n";
