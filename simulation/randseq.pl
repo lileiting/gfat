0b16e5077d4b282dee40f726a83d3e3a09df1ae0
Leiting Li <lileiting@gmail.com>
2015-06-23 11:11:40 +0800

    add an option to make random CDS sequences

--- simulation/randseq.pl
+++ simulation/randseq.pl
@@ -29,27 +29,28 @@ sub read_commands{
     my $help;
     my $num = 1;
     my $aa;
-    #my $seqlen = 100;
     my $prefix = q/RandSeq/;
     my $min = 100;
     my $max = 100;
+    my $cds;
     GetOptions(
         "n|num=i"    => \$num,
         "a|aa"       => \$aa,
         "p|prefix=s" => \$prefix,
         "I|min=i"    => \$min,
         "X|max=i"    => \$max,
+        "cds"        => \$cds,
         "h|help"     => \$help);
     usage if $help;
     die "ERROR: max length ($max), min length($min)\n" if $max < $min;
-    #$seqlen = shift @ARGV if @ARGV;
+    die "CAUTION: CDS and AA were both switched on!" if $cds and $aa;
     return {
         num    => $num,
-       # seqlen => $seqlen,
         prefix => $prefix,
         min    => $min,
         max    => $max,
-        aa     => $aa
+        aa     => $aa,
+        cds    => $cds
     };
 }
 
@@ -63,6 +64,31 @@ sub amino_acid{
         Q E G H I 
         L K M F P 
         S T W Y V/;
+}
+
+sub non_stop_codon{
+    return qw/codons
+        AAA    AAT    AAG    AAC
+        ATA    ATT    ATG    ATC
+        AGA    AGT    AGG    AGC
+        ACA    ACT    ACG    ACC
+               TAT           TAC
+        TTA    TTT    TTG    TTC
+               TGT    TGG    TGC
+        TCA    TCT    TCG    TCC
+        GAA    GAT    GAG    GAC
+        GTA    GTT    GTG    GTC
+        GGA    GGT    GGG    GGC
+        GCA    GCT    GCG    GCC
+        CAA    CAT    CAG    CAC
+        CTA    CTT    CTG    CTC
+        CGA    CGT    CGG    CGC
+        CCA    CCT    CCG    CCC
+/;
+}
+
+sub stop_codon{
+    return qw/TAA TAG TGA/;
 }
 
 sub rand_length{
@@ -88,14 +114,23 @@ sub main{
     my $min = $para->{min};
     my $max = $para->{max};
     my $aa = $para->{aa};
+    my $cds = $para->{cds};
     my $prefix = $para->{prefix};
-    my ($unit, @char) = nucleotide;
-    ($unit, @char) = amino_acid if $aa;
+    my ($unit, @char);
+    if($aa){
+        ($unit, @char) = amino_acid
+    }elsif($cds){
+        ($unit, @char) = non_stop_codon;
+    }else{
+        ($unit, @char) = nucleotide;
+    }
 
     for my $n (1..$num){
         my $length = rand_length($min, $max);
         my $seqheader = qq/$prefix$n $length$unit/;
-        my $seqstr    = format_seqstr(rand_seq($length, @char));
+        my $seqstr    = rand_seq( $cds ? $length - 2 : $length, @char);
+        $seqstr = 'ATG'.$seqstr.rand_seq(1,stop_codon) if $cds;
+        $seqstr = format_seqstr($seqstr);
         print ">$seqheader\n$seqstr\n";
     }
 }
