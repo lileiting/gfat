c96fcb76532c395e99ff9e92ccec9cdce811afe1
Leiting Li <lileiting@gmail.com>
2015-06-28 15:54:21 +0800

    add functions of visible character count and invisible character count

--- table_utils/line_utils.pl
+++ table_utils/line_utils.pl
@@ -26,6 +26,8 @@ perl $FindBin::Script CMD [OPTIONS]
   maxlen   | Max line length
 
   cc       | Character count
+  vcc      | Visible character count
+  ivcc     | Invisable character count
 
 USAGE
     exit;
@@ -42,6 +44,8 @@ sub functions_hash{
         length     => \&line_length,
         maxlen     => \&maxlen,
         cc         => \&char_count,
+        vcc        => \&visible_char_count,
+        ivcc       => \&invisible_char_count,
     );
 }
 
@@ -169,30 +173,32 @@ sub get_char_count{
     my %char;
     my $char = '';
     while(read($in_fh, $char, 1)){
-        my $ord = ord($char);
-        $char{$ord}++;
+        $char{ord($char)}++;
     }
     return %char;
 }
 
-sub decode{
-    if($_[0] >= 33 and $_[0] <= 126){
-        return chr($_[0])
-    }else{
-        return "chr($_[0])";
-    }
-}
+sub is_visible_char   { return $_[0] >= 33 && $_[0] <= 126 ? 1 : 0 }
+sub is_invisible_char { return $_[0] <  33 || $_[0] >  126 ? 1 : 0 }
+sub decode{ return is_visible_char($_[0]) ? chr($_[0]) : "chr($_[0])"; }
 
-sub char_count{
-    my ($in_fh, $out_fh) = get_fh(q/cc/);
+sub char_count_main{
+    my $cmd = shift;
+    my ($in_fh, $out_fh) = get_fh($cmd);
     my %char = get_char_count($in_fh);
 
     for my $ord (sort {$a <=> $b} keys %char){
-        my $num = $char{$ord};
+        my $count = $char{$ord};
         my $char = decode($ord);
-        print "$ord\t$char\t$num\n";
+        next if $cmd eq q/ivcc/ and is_visible_char($ord);
+        next if $cmd eq q/vcc/  and is_invisible_char($ord);
+        print $out_fh "$ord\t$char\t$count\n";
     }
 
     close_fh($in_fh, $out_fh);
 }
+
+sub char_count           { char_count_main(q/cc/)   }
+sub visible_char_count   { char_count_main(q/vcc/)  }
+sub invisible_char_count { char_count_main(q/ivcc/) }
 
