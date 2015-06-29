20906f6805f8499eb4842d75ce195bef8ddf5b1d
Leiting Li <lileiting@gmail.com>
2015-06-29 11:23:30 +0800

    use Formats::Cmd::Base

--- table_utils/line_utils.pl
+++ table_utils/line_utils.pl
@@ -4,6 +4,8 @@ use warnings;
 use strict;
 use Getopt::Long;
 use FindBin;
+use lib "$FindBin::RealBin/../lib";
+use Formats::Cmd::Base;
 use List::Util qw(sum max min);
 
 sub base_usage{
@@ -61,61 +63,6 @@ base_main() unless caller;
 ###################
 # Define commands #
 ###################
-
-#
-# Common subroutines
-#
-
-sub cmd_usage{
-    my $cmd = shift;
-    print <<USAGE;
-
-perl $FindBin::Script $cmd [OPTIONS]
-
- [-i,--input]  FILE
- -o,--output   FILE
- -h,--help
-
-USAGE
-    exit;
-}
-
-sub get_options{
-    my $cmd = shift;
-    GetOptions(
-        "input=s"  => \my $infile,
-        "output=s" => \my $outfile,
-        "help"     => \my $help
-    );
-    cmd_usage($cmd) if $help or (!$infile and @ARGV == 0 and -t STDIN);
-    my ($in_fh, $out_fh) = (\*STDIN, \*STDOUT);
-    $infile = shift @ARGV if (!$infile and @ARGV > 0);
-    open $in_fh, "<", $infile or die "$infile: $!" if $infile;
-    open $out_fh, ">", $outfile or die "$outfile: $!" if $outfile;
-
-    return {
-        in_fh => $in_fh,
-        out_fh => $out_fh
-    };
-}
-
-sub get_fh{
-    my $cmd = shift;
-    my $options = get_options($cmd);
-    my $in_fh = $options->{in_fh};
-    my $out_fh = $options->{out_fh};
-    return ($in_fh, $out_fh);
-}
-
-sub close_fh{
-    my @fhs = @_;
-    for my $fh (@fhs){
-        close $fh unless 
-               $fh eq \*STDIN  or 
-               $fh eq \*STDOUT or 
-               $fh eq \*STDERR;
-    }
-}
 
 #
 # Command win2linux, win2mac, linux2win, linux2mac, mac2win, mac2linux
