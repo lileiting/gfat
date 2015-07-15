f68066cdaaf44edd0a9949091340805e2db5357d
Leiting Li <lileiting@gmail.com>
2015-07-15 15:28:27 +0800

    simplify results from transmembrane motif prediction

---
+++ formats/tmm.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use FindBin;
+use lib "$FindBin::RealBin/../lib";
+use Gfat::Cmd::Base qw(base_main get_fh close_fh);
+
+sub actions{
+    return {
+        simplify => [ \&simplify, "Simplify results"],
+        -description => "Process data from transmembrane motif prediction [http://www.cbs.dtu.dk/services/TMHMM/]"
+    };
+}
+
+base_main(actions);
+
+##############
+# Subroutine #
+##############
+
+=head2 tmm.pl simplify
+
+  Title    : tmm.pl simplify
+
+  Functions: Simplify results from transmembrane motif prediction
+
+  Usage    : tmm.pl simplify [OPTIONS]
+
+  Options  : [-i,--input] <FILE>
+             -o,--output  FILE
+             -h,--help
+
+=cut
+
+sub simplify_topology{
+    my $str = shift;
+    die "CAUTION: Topology is empty!" 
+        unless $str =~ /^Topology=([io\-\d]+)$/;
+    $str = $1;
+    $str =~ s/^[io]//;
+    $str =~ s/[io]$//;
+    $str =~ s/[io]/,/g;
+    return $str;
+}
+
+sub simplify{
+    my ($in_fh, $out_fh) = get_fh(q/simplify/);
+    while(<$in_fh>){
+        next if /^\s*#/ or /^\s*$/;
+        my @F = split /\t/;
+        my $geneid = $F[0];
+        my $topology = simplify_topology($F[-1]);
+        print $out_fh "$geneid\t$topology\n";
+    }
+    close_fh($in_fh, $out_fh);
+}
+
