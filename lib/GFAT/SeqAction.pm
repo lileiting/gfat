47397a660695f473edc78edf2ac6b0410278a9d4
Leiting Li <lileiting@gmail.com>
2015-09-15 14:58:13 +0800

    change subroutine name from new to new_action

--- lib/GFAT/SeqAction.pm
+++ lib/GFAT/SeqAction.pm
@@ -5,7 +5,7 @@ use strict;
 use GFAT::SeqActionBase;
 
 sub acclist{
-    my $action = new(
+    my $action = new_action(
         -action      => 'acclist',
         -description => "Print ACC list for a sequence file",
     );
@@ -15,7 +15,7 @@ sub acclist{
 }
 
 sub idlist{
-    my $action = new(
+    my $action = new_action(
         -action => 'idlist'
     );
     while( my $seq = $action->{in}->next_seq){
--- lib/GFAT/SeqActionBase.pm
+++ lib/GFAT/SeqActionBase.pm
@@ -7,10 +7,10 @@ use Getopt::Long qw(:config gnu_getopt);
 use Bio::Perl;
 use parent qw(Exporter);
 use vars qw(@EXPORT @EXPORT_OK);
-@EXPORT = qw(new);
-@EXPORT_OK = qw(new);
+@EXPORT = qw(new_action);
+@EXPORT_OK = qw(new_action);
 
-sub new_usage{
+sub action_usage{
     my %args = @_;
     my $options_usage = '    '.join("\n    ", 
         sort {$a cmp $b}keys %{$args{-options}});
@@ -26,7 +26,7 @@ $options_usage
     return sub {print $usage; exit}
 }
 
-sub new{
+sub new_action{
     my %args = @_;
     my %action;
     die "Action name was not given!" unless $args{-action};
@@ -35,7 +35,7 @@ sub new{
     $args{-options}->{"help|h"} //= "Print help";
     $args{-options}->{"out|o=s"}  //= "Output file";
     #$args{'-filenumber'} //= '1+'; # 1, 2, 3, 1+
-    my $usage = new_usage(%args);
+    my $usage = action_usage(%args);
     my %options;
     GetOptions(\%options, keys %{$args{-options}});
     &{$usage} if $options{help} or (@ARGV == 0 and -t STDIN);
