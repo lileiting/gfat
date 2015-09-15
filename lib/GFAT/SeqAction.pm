6f3377246e6b71e4be9020a6bf2ea6bd941eb629
Leiting Li <lileiting@gmail.com>
2015-09-15 14:55:32 +0800

    Add GFAT::SeqActionBase

--- lib/GFAT/SeqAction.pm
+++ lib/GFAT/SeqAction.pm
@@ -2,51 +2,7 @@ package GFAT::SeqAction;
 
 use warnings;
 use strict;
-use FindBin;
-use Getopt::Long qw(:config gnu_getopt);
-use Bio::Perl;
-
-sub new_usage{
-    my %args = @_;
-    my $options_usage = '    '.join("\n    ", 
-        sort {$a cmp $b}keys %{$args{-options}});
-    my $usage = "
-USAGE
-    $FindBin::Script $args{-action} infile [OPTIONS]
-    $FindBin::Script $args{-action} [OPTIONS] infile
-
-DESCRIPTION
-    $args{-description}
-
-OPTIONS
-$options_usage
-
-";
-    return sub {print $usage; exit}
-}
-
-sub new{
-    my %args = @_;
-    my %action;
-    die "Action name was not given!" unless $args{-action};
-    $args{-description} //= $args{-action};
-    $args{-format} //= $::format;
-    $args{-options}->{"help|h"} //= "Print help";
-    $args{-options}->{"out|o=s"}  //= "Output file";
-    #$args{'-filenumber'} //= '1+'; # 1, 2, 3, 1+
-    my $usage = new_usage(%args);
-    my %options;
-    GetOptions(\%options, keys %{$args{-options}});
-    &{$usage} if $options{help} or (@ARGV == 0 and -t STDIN);
-    my ($file, $fh) = (shift @ARGV, \*STDIN);
-    if($file and $file ne '-' ){
-        open $fh, "<", $file or die "$!";
-    }
-    my $in = Bio::SeqIO->new(-fh => $fh, 
-                             -format => $args{format});
-    $action{in}=$in;
-    return \%action;
-}
+use GFAT::SeqActionBase;
 
 sub acclist{
     my $action = new(
