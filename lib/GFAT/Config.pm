321f78768418ef8fd315a13ee290fca3b4b2445c
Leiting Li <lileiting@gmail.com>
2015-09-20 19:29:31 +0800

    remove GFAT::ActionBioBase

--- lib/GFAT/Config.pm
+++ lib/GFAT/Config.pm
@@ -6,7 +6,7 @@ use base qw(Exporter);
 @EXPORT = qw(print_version);
 @EXPORT_OK = qw($VERSION $textwidth $seqwidth print_version);
 
-our $VERSION = '0.1.262';
+our $VERSION = '0.2.263';
 our $textwidth = 70;
 our $seqwidth = 60;
 
