af274e01c88b5a172e0a27fde4f349157938ee56
Leiting Li <lileiting@gmail.com>
2015-09-24 11:42:55 +0800

    recursion and arrangement

---
+++ projects/arragement.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use FindBin;
+
+sub main_usage{
+    print << "usage";
+
+USAGE
+    $FindBin::Script NUM
+
+usage
+    exit;
+}
+
+sub main{
+    main_usage unless @ARGV;
+    my $num = shift @ARGV;
+    main_usage unless $num > 1;
+    my @input = (1..$num);
+    arrangement(@input);
+}
+
+main() unless caller;
+
+############################################################
+
+sub arrangement{
+    _arrangement(0, @_);
+}
+
+my @pre = ();
+sub _arrangement{
+    my ($level, @a) = @_;
+    if(@a == 2){
+        print join(",", @pre, @a),"\n";
+        print join(",", @pre, reverse @a),"\n";
+    }
+    elsif(@a > 2){
+        for (my $i = 0; $i <= $#a; $i++){
+            my @tmp = @a;
+            my $first = splice(@tmp,$i,1);
+            $pre[$level] = $first;
+            _arrangement($level+1, @tmp);
+        }
+    }
+    else{
+        die "Number of array elements is too small: ".@a."\n";
+    }
+}
+
+__END__
