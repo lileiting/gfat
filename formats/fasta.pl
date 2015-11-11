e3eef1eb6c1d2d3e956ee3b896142486bff684b7
Leiting Li <lileiting@gmail.com>
2015-11-11 11:50:39 +0800

    add function fromtab for fasta.pl

--- formats/fasta.pl
+++ formats/fasta.pl
@@ -4,6 +4,7 @@ use warnings;
 use strict;
 use FindBin;
 use lib "$FindBin::RealBin/../lib";
+use GFAT::ActionNew;
 use GFAT::SeqAction;
 
 sub main_usage{
@@ -17,6 +18,7 @@ ACTIONS
     clean    | Clean irregular characters
     filter   | Filter sequences by size and Number of Ns or Xs
     format   | Read in and write out sequences
+    fromtab  | Convert 2-column sequence to FASTA format
     gc       | GC content
     getseq   | Get sequences by IDs
     getseq2  | Output sequences by the order of input IDs
@@ -55,5 +57,44 @@ sub main{
 }
 
 main() unless caller;
+
+sub fromtab{
+    my $args = new_action(
+        -desc => 'Convert 2-column sequence to FASTA format 
+                  [Copied function from tanghaibao\'s
+                   python -m jcvi.formats.fasta fromtab]',
+        -options => {
+            "sep|s=s" => 'Separator in the tabfile [default: tab]',
+            "replace|r=s" => 'Replace spaces in name to char 
+                              [default: none]',
+            "length|l=i" => 'Output sequence length per line 
+                             [default: unlimited]',
+            "fasta|f"    => 'A shortcut for --length=60'
+                    }
+    );
+
+    my $options = $args->{options};
+
+    my $sep     = $options->{sep} // "\t";
+    my $replace = $options->{replace};
+    my $length  = $options->{length};
+    my $fasta   = $options->{fasta};
+
+    $length = 60 if $fasta and not $length;
+
+    for my $fh (@{$args->{in_fhs}}){
+        while(<$fh>){
+            next if /^\s*$/ or /^\s*#/;
+            chomp;
+            my ($id, $seq) = split /$sep/;
+            $id =~ s/ /$replace/g if $replace;
+            if($length){
+                $seq =~ s/(.{$length})/$1\n/g;
+                chomp $seq;
+            }
+            print ">$id\n$seq\n";
+        }
+    }
+}
 
 __END__
