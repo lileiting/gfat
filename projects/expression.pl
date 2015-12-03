19f4011f777c88225f2fe45fd0db18e63de9bd67
Leiting Li <lileiting@gmail.com>
2015-12-03 16:35:23 +0800

    a script to analyse expression data

---
+++ projects/expression.pl
@@ -0,0 +1,90 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use Getopt::Long qw(:config gnu_getopt);
+use FindBin;
+use lib "$FindBin::RealBin/../lib";
+use GFAT::ActionNew;
+
+sub main_usage{
+    print <<"usage";
+
+Usage
+    $FindBin::Script ACTION [OPTIONS]
+
+Description
+    Input is a data matrix, which defined as,
+    1) frist row is sample names
+    2) first column is observation names
+    3) one row per observation
+    4) one column per sample
+
+Available Actions
+    tissue_specific
+
+usage
+    exit;
+}
+
+sub main{
+    main_usage unless @ARGV;
+    my $action = shift @ARGV;
+    if(defined &{\&{$action}}){
+        &{\&{$action}}; 
+    }
+    else{
+        die "CAUTION: action $action was not defined!\n";
+    }
+}
+
+main unless caller;
+
+###################
+# Define Actions #
+###################
+
+sub load_matrix{
+    my $args = shift;
+    my $fh = $args->{in_fhs}->[0];
+    my @matrix;
+    while(<$fh>){
+        chomp;
+     	push @matrix, [split /\t/];
+    }
+    return \@matrix;
+}
+
+sub tissue_specific{
+    my $args = new_action(
+        -desc => 'Find tissue specific genes',
+        -options => {
+            "diff|d=f" => 'Cutoff of difference value between the best NFPKM
+                           and the second best NFPKM [default: 0.01]',
+            "min|i=f"  => 'Cutoff value for the best NFPKM [default: 0]'
+        }
+    );
+    my $diff = $args->{options}->{diff} // 0.01;
+    my $min  = $args->{options}->{min} // 0;
+    
+    my $matrix = load_matrix $args;
+    my @tissues = @{$matrix->[0]};
+    for (my $i = 1; $i <= $#{$matrix}; $i++){
+        my @array = @{$matrix->[$i]};
+        my $gene_id = $array[0];
+        my @index = sort{$array[$b] <=> $array[$a]}(1..$#array);
+        my $best_nfpkm = $array[$index[0]];
+        my $best_tissue = $tissues[$index[0]];
+        my $second_nfpkm = $array[$index[1]];
+        next unless $best_nfpkm - $second_nfpkm > $diff;
+        next unless $best_nfpkm > $min;
+        print join("\t", $gene_id, 
+                         $best_tissue, 
+                         $best_nfpkm, 
+                         $second_nfpkm)."\n";
+    }
+    
+}
+
+__END__
+
