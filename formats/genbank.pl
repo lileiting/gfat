aabdccb52288a09c4f788d2525af130cbe99a754
Leiting Li <lileiting@gmail.com>
2016-03-01 22:05:28 +0800

    a script to print primer sequences in genbank file

---
+++ formats/genbank.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use FindBin;
+use Bio::Perl;
+
+sub main_usage{
+    print <<"usage";
+
+    $FindBin::Script <input.gb>
+
+usage
+    exit;
+}
+
+sub main{
+    main_usage unless @ARGV == 1;
+    my $input = shift @ARGV;
+    my $in = Bio::SeqIO->new(-file => $input, -format => 'genbank');
+    while(my $seq = $in->next_seq){
+        my $seq_id = $seq->display_id;
+        my $desc = $seq->desc;
+        print qq/$desc\n/ and next 
+            unless $desc =~ /satellite.+?((Tsu|EMPc|NH|NB|BGT)\w+)/i;
+        my $ssr_id = $1;
+        my @primer_seqs;
+        my @features = $seq->get_SeqFeatures();
+        foreach my $feat ( @features ) {
+            next unless $feat->primary_tag eq 'primer_bind';
+            push @primer_seqs, $feat->seq->seq;
+        }
+
+        print join("\t", $seq_id, $ssr_id, @primer_seqs)."\n";
+
+    }
+}
+
+main unless caller;
