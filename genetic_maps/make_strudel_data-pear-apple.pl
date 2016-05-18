cf7d03b566f4737200430040f050c7c6cd5530a3
Leiting Li <lileiting@gmail.com>
2016-05-18 11:22:26 +0800

    update

--- genetic_maps/make_strudel_data-pear-apple.pl
+++ genetic_maps/make_strudel_data-pear-apple.pl
@@ -29,9 +29,11 @@ sub convert_map_data_to_strudel_data{
     my $map1_map_id;
     open my $map1_fh, $map1_file or die $!;
     my %map1;
+    my @data1;
     while(<$map1_fh>){
         chomp;
         my ($map_id, $LG, $marker, $pos) = split /\t/;
+        push @data1, [$map_id, $LG, $marker, $pos];
         unless($map1_map_id){
             $map1_map_id = $map_id
         }
@@ -40,14 +42,18 @@ sub convert_map_data_to_strudel_data{
                 "found two: $map1_map_id, $map_id" 
                 if $map1_map_id ne $map_id
         }
-        
         $map1{$marker} = $LG;
+
+    }
+    close $map1_fh;
+    
+    for (sort {$a->[1] <=> $b->[1] or $a->[3] <=> $b->[3]} @data1){
+        my ($map_id, $LG, $marker, $pos) = @$_;
         print join("\t", 'feature', $map_id, 
                          "LG$LG", $marker, 'marker', 
                          sprintf("%.1f", $pos))."\n";
     }
-    close $map1_fh;
-
+    
     my %map2;
     open my $map2_fh, $map2_file or die $!;
     while(<$map2_fh>){
@@ -62,7 +68,7 @@ sub convert_map_data_to_strudel_data{
 
     for my $marker1 (sort {$a cmp $b} keys %map1){
         for my $marker2 (sort {$a cmp $b} keys %map2){
-            if($marker1 eq $marker2){
+            if(is_homolog($marker1, $marker2)){
                 for my $map_id (sort {$a cmp $b}keys %{$map2{$marker2}}){
                     print join("\t", 'homolog',
                         $map1_map_id, $marker1, $map_id,
@@ -74,3 +80,11 @@ sub convert_map_data_to_strudel_data{
     }
 }
 
+sub is_homolog{
+    my ($marker1, $marker2) = @_;
+    return 1 if $marker1 eq $marker2;
+    $marker1 =~ s/-\d$//;
+    $marker2 =~ s/-\d$//;
+    return 1 if $marker1 eq $marker2;
+    return 0;
+}
