8ae3a3328bb40dfc82db8de4f69dc4d3811b9d85
Leiting Li <lileiting@gmail.com>
2016-07-26 19:18:18 +0800

    [apps:fetch:getsequence] add options: --database, --format, --listfile

--- apps/fetch.pl
+++ apps/fetch.pl
@@ -6,13 +6,37 @@ use FindBin;
 use lib "$FindBin::RealBin/../lib";
 use GFAT::ActionNew;
 use Bio::Perl;
+use Text::Abbrev;
 
-sub genbank{
+sub getsequence{
     my $args = new_action(
-        -desc => 'Fetch sequences from genbank, eg. NM_022163.3, GI:169658372'
+        -desc => 'Fetch sequences from remote databases like genbank by
+            provided accession numbers, e.g. NM_022163.3, GI:169658372',
+        -options => {
+            "database|d=s" => "Database: Swissprot ('swiss'), EMBL ('embl'),
+                GenBank('genbank'), GenPept ('genpept'), and RefSeq ('refseq')
+                [default: genbank]",
+            "format|f=s" => 'Output sequence formats: ace, asciitree, chaosxml,
+                embl, fasta, gcg, genbank, pir, raw, swiss,tab [default: fasta]
+                See: http://bioperl.org/howtos/SeqIO_HOWTO.html',
+            "listfile|l=s" => 'A list file providing accession numbers one per
+                line, or in first column of a tab file'
+        }
     );
-    my $out = Bio::SeqIO->new(-fh => $args->{out_fh}, -format=>'fasta');
+    my $database = $args->{options}->{database} // 'genbank';
+    my $format = $args->{options}->{format} // 'fasta';
+    my $listfile = $args->{options}->{listfile};
+    my $out = Bio::SeqIO->new(-fh => $args->{out_fh}, -format => $format);
     my @terms = @{$args->{infiles}};
+    if($listfile){
+        open my $list_fh, $listfile or die "$!: $listfile\n";
+        while(<$list_fh>){
+            next if /^\s*$/ or /^\s*#/;
+            chomp;
+            push @terms, (split /\t/)[0];
+        }
+        close $list_fh;
+    }
     for my $term (@terms){
         my $seq = get_sequence('genbank',$term);
         $out->write_seq($seq);
@@ -22,7 +46,7 @@ sub genbank{
 
 sub main{
     my %actions = (
-        genbank => 'Fetch sequences from genbank',
+        getsequence => 'Fetch sequences from genbank',
     );
     &{\&{run_action(%actions)}};
 }
