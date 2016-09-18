676e6cc36b492d5143a1400a13c69729a4c293ef
Leiting Li <lileiting@gmail.com>
2016-09-18 19:31:24 +0800

    chisqtest

--- lib/GFAT/Chisquare.pm
+++ lib/GFAT/Chisquare.pm
@@ -1,10 +1,12 @@
 package GFAT::Chisquare;
 
+use warnings;
+use strict;
 use Carp qw< croak >;
 use List::Util qw< sum >;
 use Statistics::Distributions qw< chisqrprob >;
 our @ISA = qw(Exporter);
-our @EXPORT = qw(chisqtest_of_lmxll chisqtest_of_nnxnp chisqtest_of_hkxhk);
+our @EXPORT = qw(chisqtest);
 our @EXPORT_OK = @EXPORT;
 
 sub chi_squared_test {
@@ -27,30 +29,42 @@ sub chi_squared_test {
 #  observed => [16, 5, 9, 7, 6, 17],
 #  expected => [(10) x 6];
 
-sub chisqtest_of_lmxll{
-    die unless @_ == 2;
-    my ($obs1, $obs2) = @_;
-    my $avg = ($obs1 + $obs2) / 2;
-    return chi_squared_test(observed => [$obs1, $obs2], 
-        expected => [$avg, $avg]);
-}
+sub chisqtest {
+    my $seg_type = shift;
+    my @obs = @_;
+    croak q(Two numbers required for lmxll and nnxnp) 
+        if ($seg_type eq 'lmxll' or $seg_type eq 'nnxnp') and not @obs == 2;
+    croak q(Three numbers required for hkxhk) 
+        if $seg_type eq 'hkxhk' and not @obs == 3;
+    croak q(Four numbers required for efxeg and abxcd) 
+        if ($seg_type eq 'efxeg' or $seg_type eq 'abxcd') and not @obs == 4;
 
-sub chisqtest_of_nnxnp{
-    die unless @_ == 2;
-    my ($obs1, $obs2) = @_;
-    my $avg = ($obs1 + $obs2) / 2;
-    return chi_squared_test(observed => [$obs1, $obs2], 
-        expected => [$avg, $avg]);
-}
-
-sub chisqtest_of_hkxhk{
-    die unless @_ == 3;
-    my ($obs1, $obs2, $obs3) = @_;
-    my $half = ($obs1 + $obs2 + $obs3) / 2;
+    my $half = sum(@obs) / 2;
     my $quarter = $half / 2;
-    return chi_squared_test(observed => [$obs1, $obs2, $obs3], 
-        expected => [$quarter, $half, $quarter]);
-}
 
+    if($seg_type eq 'lmxll' or $seg_type eq 'nnxnp'){
+        return chi_squared_test(
+            observed => [@obs],
+            expected => [$half, $half]
+        );
+    }
+    elsif($seg_type eq 'hkxhk'){
+        return chi_squared_test(
+            observed => [@obs],
+            expected => [$quarter, $half, $quarter]
+        );
+    }
+    elsif($seg_type eq 'efxeg' or $seg_type eq 'abxcd'){
+        return chi_squared_test(
+            observed => [@obs],
+            expected => [$quarter, $quarter, $quarter, $quarter]
+        );
+    }
+    else{
+        croak qq(Wrong segregation type: $seg_type! 
+            Supported segregation types are lmxll, 
+            nnxnp, hkxhk, efxeg and abxcd);
+    }
+}
 
 1;
---
+++ t/test-chisqure.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use FindBin;
+use lib "$FindBin::RealBin/../lib";
+use GFAT::Chisquare;
+
+#print GFAT::Chisquare->chi_squared_test(
+#  observed => [16, 5, 9, 7, 6, 17],
+#  expected => [(10) x 6]), "\n";
+
+#print GFAT::Chisquare->chi_squared_test(observed => [60,80], expected => [70,70]),"\n";
+
+print chisqtest('lmxll', 60, 80), "\n";
+print chisqtest('nnxnp', 60, 80), "\n";
+print chisqtest('hkxhk', 30, 80, 40), "\n";
+print chisqtest('nnxnp', 60, 100), "\n";
+print chisqtest('lmxll', 40, 100), "\n";
+print chisqtest('lmxll', 30, 100), "\n";
+print chisqtest('efxeg', 40, 50, 60, 30), "\n";
+print chisqtest('abxcd', 30, 50, 20, 55), "\n";
+
+
