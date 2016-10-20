8edebd944468dab4dbbba430113ec57bdb719aed
Leiting Li <lileiting@gmail.com>
2016-10-20 15:26:49 +0800

    add a wrong-type test

--- t/test-chisqure.pl
+++ t/test-chisqure.pl
@@ -21,4 +21,5 @@ print chisqtest('lmxll', 30, 100), "\n";
 print chisqtest('efxeg', 40, 50, 60, 30), "\n";
 print chisqtest('abxcd', 30, 50, 20, 55), "\n";
 
+print chisqtest('abxcde', 30, 50, 20, 55), "\n";
 
